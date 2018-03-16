d64863347169db050d08cce9e69e76b6227fecb8
Madison <madisonlkain@gmail.com>
2018-03-16 15:54:36 -0600

    updated form with inputs and buttons

--- shelfie/server/index.js
+++ shelfie/server/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const massive = require('massive');
+require('dotenv').config();
+
 const port = 3005;
 
 const app = express();
 app.use(bodyParser.json());
+
+massive( process.env.DATABASE_URI).then( db => {
+        console.log( 'Connected to Database!' );
+        app.set('db', db)
+    });
+
 
 app.listen(port, () => console.log(`Searching for Rebel Scum on port ${port}`));
--- shelfie/src/components/Form/Form.js
+++ shelfie/src/components/Form/Form.js
@@ -4,15 +4,43 @@ export default class Form extends Component {
     constructor() {
         super()
         this.state = {
-
+           imageURL: '',
+           productName: '',
+           price: 0 
         }
+    }
+
+    updateState(prop, val){
+        this.setState({
+            [prop]: val
+        })
+    }
+
+    clearInput(){
+        console.log(this.state)
+        this.setState({
+            imageURL: '',
+            productName: '',
+            price: 0
+        })
     }
 
     render() {
         return(
-                
             <div>
-                Form
+                <input onChange={(e)=>this.updateState('imageURL', e.target.value)}
+                        placeholder='Image URL'
+                />
+                <input onChange={(e)=>this.updateState('productName', e.target.value)}
+                        placeholder='Product Name'
+                />
+                <input onChange={(e)=>this.updateState('price', e.target.value)}
+                        placeholder='Price'
+                />
+                <div>
+                    <button onClick={() => this.clearInput()}>Cancel</button>
+                    <button>Add to Inventory</button>
+                </div>
             </div>
         )
     }
